331279994b400249dc3fb17262252232c3f35ff1
Patricio <ijpatricio@gmail.com>
2024-10-30 09:26:46 +0000

    Add custom theme

---
+++ app/Filament/Pages/DaisyPage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class DaisyPage extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.daisy-page';
+}
